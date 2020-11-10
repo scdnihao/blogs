2b19f743a26824e9ba643ead2302a9e1ab3d4a6f
SCDnihao <sunchendong@firsttech-soft.com>
2020-11-10 17:03:45 +0800

    测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<a href="./home.html">GO home.html</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ src/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="home">
+		<div id="home">
+			<a href="./index.html">GO index.html</a>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home',
+		components: {}
+	}
+</script>
+
+<style>
+</style>
