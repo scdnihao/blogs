dcc9ed7d4efca4ec0f438fbe7c13f8deaa08882f
SCDnihao <sunchendong@firsttech-soft.com>
2020-11-10 18:23:30 +0800

    HOME.VUE

--- src/home/home.vue
+++ src/home/home.vue
@@ -1,17 +1,27 @@
 <template>
 	<div id="home">
-		<div id="home">
+		<div>
 			<a href="./index.html">GO index.html</a>
 		</div>
+		<HelloWorld></HelloWorld>
 	</div>
 </template>
 
 <script>
+	import HelloWorld from '../components/HelloWorld'
 	export default {
 		name: 'Home',
-		components: {}
+		components: {
+			HelloWorld
+		}
 	}
 </script>
 
 <style>
+	#home{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+	}
 </style>
